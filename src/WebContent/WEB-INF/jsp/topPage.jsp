b11354481a9327ecd619540aff4dfa9565f78f2d
dojo2022-sumikawa <sumikawa-haruki@seplus2016.onmicrosoft.com>
2022-06-10 13:38:37 +0900

    topPage

--- src/WebContent/WEB-INF/jsp/topPage.jsp
+++ src/WebContent/WEB-INF/jsp/topPage.jsp
@@ -4,9 +4,41 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Forza｜TopPage</title>
 </head>
 <body>
-
+<div class="wrapper">
+<!-- ヘッダ－ -->
+		<header class="header">
+            <div class="logo">
+                <img src="/Forza/images/Forza_trans.png" alt="home">
+            </div>
+            <div class="news">
+                <h3>お知らせはここに表示</h3>
+            </div>
+            <div class="myPage">
+                <input type="button" id="btn" value="myPage">
+            </div>
+		</header>
+<!-- メイン -->
+	<main class="main">
+        <div class="calendar">
+            カレンダー
+        </div>
+        <div class="task">
+            タスクリスト
+        </div>
+        <div class="memo">
+            メモ
+        </div>
+        <div class="trophy">
+            トロフィー
+        </div>
+</main>
+<!-- フッタ－ -->
+	<footer>
+        <!-- 今回は無し -->
+	</footer>
+</div>
 </body>
 </html>
--- src/src/servlet/TopServlet.java
+++ src/src/servlet/TopServlet.java
@@ -1,11 +1,14 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class TopServlet
@@ -13,21 +16,24 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TopServlet")
 public class TopServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TopServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		/*
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/Forza/LoginServlet");
+			return;
+		}
+		*/
+
+		// トップページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/topPage.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 	/**
