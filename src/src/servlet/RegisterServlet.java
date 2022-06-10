8299dbbb3a378b089f0fe1ebf44fc0ec859a3e4b
dojo2022sawa <103237555+dojo2022sawa@users.noreply.github.com>
2022-06-10 14:23:36 +0900

    Dao

--- src/src/servlet/RegisterServlet.java
+++ src/src/servlet/RegisterServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,8 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.IdpwDAO;
 import dao.UsersDAO;
 import model.Users;
+import model.Idpw;
 import model.Result;
 
 /**
@@ -32,16 +35,37 @@ public class RegisterServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 検索ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/userRegist.jsp");
+				dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 
-}
+
+				// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String id = request.getParameter("id");
+				String password = request.getParameter("password");
+
+				// 登録処理を行う
+				UsersDAO bDao = new UsersDAO();
+				if (bDao.insert(new Users(id, password))) {	// 登録成功
+					request.setAttribute("result",
+					new Result("登録成功！", "レコードを登録しました。", "/Forza/TopServlet"));
+				}
+				else {												// 登録失敗
+					request.setAttribute("result",
+					new Result("登録失敗！", "レコードを登録できませんでした。", "/Forza/TopServlet"));
+				}
+
+				// 結果ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+				dispatcher.forward(request, response);
+			}
+		}
+
