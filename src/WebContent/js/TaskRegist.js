d5aa0eab3cf9c989427e20ee83ce36803c5b8a09
dojo2022-Itagaki <103239285+dojo2022-Itagaki@users.noreply.github.com>
2022-06-13 11:22:50 +0900

    タスク追加のポップアップ関連

--- src/WebContent/js/TaskRegist.js
+++ src/WebContent/js/TaskRegist.js
@@ -1,3 +1,11 @@
-/**
- * 
- */
+//タスク追加・編集のポップアップに関するJS
+
+const popup = document.getElementById('popup'); //＋ボタン
+    const close = document.getElementById('close'); //「閉じる」ボタン
+    const popWin = document.getElementById('popWin'); //ポップアップしてくるウィンドウ(登録画面)
+    popup.addEventListener('click', () => {
+    popWin.style.display = 'flex';
+    });
+    close.addEventListener('click', () => {
+    popWin.style.display = 'none';
+    });
