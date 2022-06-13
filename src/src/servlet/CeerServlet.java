d7d900a24cea098e7676fc4effcdf87c801ee08b
dojo2022-Itagaki <103239285+dojo2022-Itagaki@users.noreply.github.com>
2022-06-13 10:45:57 +0900

    CeerServlet

--- src/src/servlet/CeerServlet.java
+++ src/src/servlet/CeerServlet.java
@@ -1,19 +1,25 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.TaskListsDao;
+import model.Result;
+import model.Task;
+
 /**
- * Servlet implementation class CeerServlet
+ * Servlet implementation class RegisterServlet
  */
-@WebServlet("/CeerServlet")
+@WebServlet("/RegisterServlet")
 public class CeerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +32,43 @@ public class CeerServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 検索ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/taskRegist.jsp");
+				dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 
-}
+
+				// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String user_id = request.getParameter("USER_ID");
+		String task_id = request.getParameter("TASK_ID");
+		String customset_id = request.getParameter("CUSTOMSET_ID");
+		String task_title = request.getParameter("TASK_TITLE");
+		String task_memo = request.getParameter("TASK_MEMO");
+		String task_date = request.getParameter("TASK_DATE");
+		String priority = request.getParameter("PRIORITY");
+		String task_judge = request.getParameter("TASK_JUDGE");
+
+				// 登録処理を行う
+				TaskListsDao TDao = new TaskListsDao();
+				if (TDao.insert(new Task(user_id,task_id,customset_id,task_title,task_memo,task_date,priority,task_judge))) {	// 登録成功
+					request.setAttribute("result",
+					new Result("登録成功！", "レコードを登録しました。", "/Forza/TopServlet"));
+				}
+				else {												// 登録失敗
+					request.setAttribute("result",
+					new Result("登録失敗！", "レコードを登録できませんでした。", "/Forza/TopServlet"));
+				}
+
+				// 結果ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/topPage.jsp");
+				dispatcher.forward(request, response);
+			}
+		}
+
