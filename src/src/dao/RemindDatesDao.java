742f4b44a9edf798d4f26018ac2513c97f125f55
dojo2022-sumikawa <sumikawa-haruki@seplus2016.onmicrosoft.com>
2022-06-14 17:39:56 +0900

    RemindDAO

--- src/src/dao/RemindDatesDao.java
+++ src/src/dao/RemindDatesDao.java
@@ -25,7 +25,7 @@ public class RemindDatesDao {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
 
 			// SQL文を準備する
-			String sql = "select USER_ID REMIND_NAME REMIND_DATE from Remind WHERE USER_ID = ?  ORDER BY ID";
+			String sql = "select * from Remind_days WHERE USER_ID = ?  ORDER BY ID";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
@@ -69,8 +69,8 @@ public class RemindDatesDao {
 		return remindList;
 	}
 
-	// 引数salaryで給料更新
-	public boolean salary(Remind salary) {
+	// 引数changeで日付更新
+	public boolean change(Remind change) {
 		Connection conn = null;
 		boolean result = false;
 
@@ -81,15 +81,80 @@ public class RemindDatesDao {
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
 
+			// 給料日の更新
+			if(change.getRemind_name() == "給料日") {
+				System.out.println("値：" + change.getRemind_name());
 			// SQL文を準備する
-			String sql = "update Remind_date set Remind_date = ? from remind_dates where USER_ID = ?";
+			String sql = "update Remind_days set Remind_date = ? where USER_ID = ? ";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-			pStmt.setString(1, salary.getUser_id());
+			java.util.Date date1 = change.getRemind_date();
+			String strDate1 = date1.toString();
+			String[] dates = strDate1.split("-");
+
+			int intDate = Integer.parseInt(dates[2]);
+			int intMonth = Integer.parseInt(dates[1]);
+			int intYear = Integer.parseInt(dates[0]);
+
+			if(intMonth < 12) {
+				intMonth ++;
+			}else {
+				intYear ++;
+			}
+
+			dates[2] = String.valueOf(intDate);
+			dates[1] = String.valueOf(intMonth);
+			dates[0] = String.valueOf(intYear);
+
+			String strDate2 = String.join("-", dates);
+			Date sqlDate = java.sql.Date.valueOf(strDate2);
+
+			pStmt.setDate(1, sqlDate);
+
+			pStmt.setString(2, change.getUser_id());
 
 			// SQL文を実行する
-			if (pStmt.executeUpdate() == 1) {
-				result = true;
+			pStmt.executeUpdate();
+			}
+
+			// 誕生日の更新
+			if(change.getRemind_name() == "誕生日") {
+			// SQL文を準備する
+			String sql = "update Remind_days set Remind_date = ? where USER_ID = ? ";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			java.util.Date date1 = change.getRemind_date();
+			String strDate1 = date1.toString();
+			String[] dates = strDate1.split("-");
+
+			int intDate = Integer.parseInt(dates[2]);
+			int intMonth = Integer.parseInt(dates[1]);
+			int intYear = Integer.parseInt(dates[0]);
+
+			intYear ++;
+
+			dates[2] = String.valueOf(intDate);
+			dates[1] = String.valueOf(intMonth);
+			dates[0] = String.valueOf(intYear);
+
+			String strDate2 = String.join("-", dates);
+			Date sqlDate = java.sql.Date.valueOf(strDate2);
+
+			pStmt.setDate(1, sqlDate);
+
+			pStmt.setString(2, change.getUser_id());
+
+			// SQL文を実行する
+			pStmt.executeUpdate();
+			}
+			while (rs.next()) {
+				Remind list = new Remind(
+				rs.getString("USER_ID"),
+				rs.getString("REMIND_NAME"),
+				rs.getDate("REMIND_DATE")
+				);
+
+				remindList.add(list);
 			}
 		}
 		catch (SQLException e) {
@@ -111,97 +176,10 @@ public class RemindDatesDao {
 		}
 
 		// 結果を返す
-		return result;
-	}
-	// 引数holidayで休日更新
-	public boolean holiday(Remind holiday) {
-		Connection conn = null;
-		boolean result = false;
-
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("org.h2.Driver");
-
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-
-			// SQL文を準備する
-			String sql = "dateadd(month,1,?) where USER_ID = ?";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-
-			pStmt.setString(1, holiday.getUser_id());
-
-			// SQL文を実行する
-			if (pStmt.executeUpdate() == 1) {
-				result = true;
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		finally {
-			// データベースを切断
-			if (conn != null) {
-				try {
-					conn.close();
-				}
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		// 結果を返す
-		return result;
-	}
-	// 引数birthdayで誕生日更新
-	public boolean birthday(Remind birthday) {
-		Connection conn = null;
-		boolean result = false;
-
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("org.h2.Driver");
-
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-
-			// SQL文を準備する
-			String sql = "select adddate where USER_ID = ?";
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-
-			pStmt.setString(1, birthday.getUser_id());
-
-			// SQL文を実行する
-			if (pStmt.executeUpdate() == 1) {
-				result = true;
-			}
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		finally {
-			// データベースを切断
-			if (conn != null) {
-				try {
-					conn.close();
-				}
-				catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		// 結果を返す
-		return result;
+		return remindList;
 	}
 
+	// 引数chooseで今日より前の日付を選択
 	public List<Remind> choose(Remind choose){
 		Connection conn = null;
 		List<Remind> remindList = new ArrayList<Remind>();
@@ -214,7 +192,7 @@ public class RemindDatesDao {
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
 
 			// SQL文を準備する
-			String sql = "select USER_ID REMIND_NAME REMIND_DATE from Remind WHERE USER_ID = ? AND REMIND_DATE <= ? ORDER BY ID";
+			String sql = "select * from Remind_days WHERE USER_ID = ? AND REMIND_DATE <= ? ORDER BY ID";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
