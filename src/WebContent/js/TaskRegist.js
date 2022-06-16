aa4b764c3f230ff92fb90f68572d2d2e437d0805
dojo2022-tanabe <tanabe-akihisa@seplus2016.onmicrosoft.com>
2022-06-16 14:57:37 +0900

    Update TaskRegist.js

--- src/WebContent/js/TaskRegist.js
+++ src/WebContent/js/TaskRegist.js
@@ -1,7 +1,30 @@
-$(".open").on("click", function(){
-  $(".popup-overlay, .popup-content").addClass("active");
-});
+    function recalc() {
+		const now = new Date();
+		const year = now.getFullYear();
+		let month = now.getMonth();
+		const date = now.getDate();
 
-$(".close, .popup-overlay").on("click", function(){
-  $(".popup-overlay, .popup-content").removeClass("active");
-});
+		month = month +1;
+
+		document.getElementById('date').textContent
+		= year + '年' + month + '月' + date + '日' + 'のタスク';
+	} recalc();
+
+
+	function onButtonClick(){
+		const clickBtn = document.getElementById('click-btn');
+		const popupWrapper = document.getElementById('popup-wrapper');
+		const close = document.getElementById('close');
+
+		// ボタンをクリックしたときにポップアップを表示させる
+		clickBtn.addEventListener('click', () => {
+		  popupWrapper.style.display = "block";
+		});
+
+		// ポップアップの外側又は「x」のマークをクリックしたときポップアップを閉じる
+		popupWrapper.addEventListener('click', e => {
+		  if (e.target.id === popupWrapper.id || e.target.id === close.id) {
+		    popupWrapper.style.display = 'none';
+		  }
+		});
+}
