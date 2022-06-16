383b60217e4d48ba6a0329907697acc9ef8a20c2
dojo2022-moriwaki <103239277+dojo2022-moriwaki@users.noreply.github.com>
2022-06-16 17:34:06 +0900

    a

--- src/WebContent/js/Trophy.js
+++ src/WebContent/js/Trophy.js
@@ -5,7 +5,7 @@ let url = new URL(window.location.href);
 // URLSearchParamsオブジェクトを取得
 let params = url.searchParams;
 
-/*ログイン日数をYとする*/
+/*ログイン日数をYとする。パラメータを受け取る*/
 var y=params.get('loginCount');
 /*タスクカウントをXとする*/
 var x=999;
