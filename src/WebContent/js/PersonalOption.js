421f64d2ec728b19d0fdb5375674426f0833acc7
dojo2022-sumikawa <sumikawa-haruki@seplus2016.onmicrosoft.com>
2022-06-21 17:06:04 +0900

    b

--- src/WebContent/js/PersonalOption.js
+++ src/WebContent/js/PersonalOption.js
@@ -24,3 +24,56 @@
 		fileReader.readAsDataURL(obj.files[0]);
 		console.log(fileReader.result) // ← (確認用)null
 	}
+
+	function update(){
+		alert("functionはいったよ！");
+
+		//入力値を取得してくる
+		let setPw = document.getElementById('pw').value;
+		let setIcon = document.getElementById('img').value;
+		let setSalary = document.getElementById('salaryDay').value;
+		let setSun = document.getElementById('sunday').value;
+		let setMon = document.getElementById('monday').value;
+		let setTue = document.getElementById('tuesday').value;
+		let setWed = document.getElementById('wednesday').value;
+		let setThu = document.getElementById('thursday').value;
+		let setFri = document.getElementById('friday').value;
+		let setSat = document.getElementById('saturday').value;
+		let setBirthMonth = document.getElementById('birthMonth').value;
+		let setBirthDay = document.getElementById('birthDay').value;
+
+		//{変数名：中に入れるもの}みたいに書いて、複数の値をpostData変数に格納
+		let postData = {Pw:setPw, Icon:setIcon, Salary:setSalary, Sun:setSun,
+		Mon:setMon, Tue:setTue, Wed:setWed, Thu:setThu, Fri:setFri, Sat:setSat,
+		BirthMonth:setBirthMonth, BirthDay:setBirthDay}
+
+
+		//非同期通信始めるよ
+		$.ajaxSetup({scriptCharset:'utf-8'});
+		$.ajax({
+			//どのサーブレットに送るか
+			//ajaxSampleのところは自分のプロジェクト名に変更する必要あり。
+			url: '/Forza/PersonalOptionServlet',
+			//どのメソッドを使用するか
+			type:"POST",
+			//受け取るデータのタイプ
+			dataType:"json",
+			//何をサーブレットに飛ばすか（変数を記述）
+			data: postData,
+			//この下の２行はとりあえず書いてる（書かなくても大丈夫？）
+			processDate:false,
+			timeStamp: new Date().getTime()
+		})
+		//非同期通信が成功したときの処理
+		.done(function(data) {
+			alert("成功1");
+			// アイコンの表示を変える。リマインドのデフォルトを変更する。
+			document.getElementById("iconImage").src=data[0];
+		})
+		//非同期通信が失敗したときの処理
+		.fail(function() {
+			//失敗とアラートを出す
+			alert("失敗！");
+		});
+	}
+
