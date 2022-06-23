f9ca7b83f6855c56305002bba6fa6e4cedc970ba
dojo2022-tanabe <tanabe-akihisa@seplus2016.onmicrosoft.com>
2022-06-23 16:46:31 +0900

    Update TaskRegistServlet.java

--- src/src/servlet/TaskRegistServlet.java
+++ src/src/servlet/TaskRegistServlet.java
@@ -1,16 +1,15 @@
+
 package servlet;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import dao.TaskListsDao;
 import model.Task;
@@ -30,7 +29,34 @@ public class TaskRegistServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		/*HttpSession session = request.getSession();
+		if (session.getAttribute("user_id") == null) {
+			response.sendRedirect("/Forza/LoginServlet");
+			return;
+		}*/
 
+		// 検索処理を行う
+		TaskListsDao TaskDao = new TaskListsDao();
+		List<Task> list = TaskDao.show();
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("lists", list);
+
+		// テンポラリーページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/taskList.jsp");
+		dispatcher.forward(request, response);
+
+
+
+        // １：タスクリストの「task_title」がクリックされる
+        // ２：一回トップ画面に戻る？
+        // ３：追加編集のポップアップが表示される?
+		// RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/topPage.jsp");
+		// dispatcher.forward(request, response);
+
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -45,55 +71,36 @@ public class TaskRegistServlet extends HttpServlet {
 
 				// リクエストパラメータを取得する
 				request.setCharacterEncoding("UTF-8");
-		        response.setContentType("application/json");
-				response.setHeader("Cache-Control", "nocache");
-				response.setCharacterEncoding("utf-8");
-
-
-				String id = request.getParameter("Id");
-				String user_id = request.getParameter("User_id");
-				String task_id = request.getParameter("Task_id");
+				String id = request.getParameter("id");
+				String user_id = request.getParameter("user_id");
+				String task_id = request.getParameter("task_id");
 				//Integer.parseInt("task_id");
-				String customset_id = request.getParameter("Customset_id");
-				String task_name = request.getParameter("Task_name");
-				String task_memo = request.getParameter("Task_memo");
-				String task_date = request.getParameter("Task_date");
-				String priority = request.getParameter("Priority");
-				String task_judge = request.getParameter("Task_judge");
+				String customset_id = request.getParameter("customset_id");
+				String task_name = request.getParameter("task_name");
+				String task_memo = request.getParameter("task_memo");
+				String task_date = request.getParameter("task_date");
+				String priority = request.getParameter("priority");
+				String task_judge = request.getParameter("task_judge");
 
-				ArrayList<Task> taskLists = new ArrayList<>();
 
 				// 登録処理を行う
-				if(taskLists!= null) {
 				TaskListsDao LDAO = new TaskListsDao();
-				LDAO.insert(new Task(id,user_id,task_id,customset_id,task_name,task_memo,task_date,priority,task_judge)); 	// 登録成功
-				Task task_data = LDAO.select(new Task(id,user_id,task_id,customset_id,task_name,task_memo,task_date,priority,task_judge));
-				taskLists.add(task_data);
-	}
-
-				/*request.setAttribute("result",
-					new Result("登録成功！", "レコードを登録しました。", "/Forza/TaskServlet"));*/
-					// 登録失敗
+				if (LDAO.insert(new Task(id,user_id,task_id,customset_id,task_name,task_memo,task_date,priority,task_judge))) {	// 登録成功
 					/*request.setAttribute("result",
-					new Result("登録失敗！", "レコードを登録できませんでした。", "/Forza/TaskServlet"));
-*/
-				try {
-					//JavaオブジェクトからJSONに変換
-					//JSONの出力
-					if(taskLists != null) {
-						ObjectMapper mapper = new ObjectMapper();
-						String json = mapper.writeValueAsString(taskLists);
-					    response.getWriter().write(json);
-					}
-					} catch (JsonProcessingException e) {
-					    e.printStackTrace();
-					}
+					new Result("登録成功！", "レコードを登録しました。", "/Forza/TaskServlet"));*/
+				}
+				else {												// 登録失敗
+					/*request.setAttribute("result",
+					new Result("登録失敗！", "レコードを登録できませんでした。", "/Forza/TaskServlet"));*/
+				}
+				TaskListsDao TaskDao = new TaskListsDao();
+				List<Task> list = TaskDao.show();
 
+				// 検索結果をリクエストスコープに格納する
+				request.setAttribute("lists", list);
 				// 結果ページにフォワードする
-				response.setContentType("application/json");
-				response.setHeader("Cache-Control", "nocache");
-				response.setCharacterEncoding("utf-8");
-
-
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/taskList.jsp");
+				dispatcher.forward(request, response);
 	}
+
 }
